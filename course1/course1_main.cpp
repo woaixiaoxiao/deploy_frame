ae86088769d2409553601758815f7189eb07a5e2
dcao <2191279878@qq.com>
2023-12-31 09:39:45 +0800

    完成hw1

--- course1/course1_main.cpp
+++ course1/course1_main.cpp
@@ -2,15 +2,17 @@
 // Created by fss on 23-5-27.
 //
 
-#include <gtest/gtest.h>
 #include <glog/logging.h>
+#include <gtest/gtest.h>
 
 int main(int argc, char *argv[]) {
-  testing::InitGoogleTest(&argc, argv);
-  google::InitGoogleLogging("Kuiper");
-  FLAGS_log_dir = "../../course1/log";
-  FLAGS_alsologtostderr = true;
 
-  LOG(INFO) << "Start test...\n";
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    google::InitGoogleLogging("Kuiper");
+    FLAGS_log_dir = "../../course1/log";
+    FLAGS_alsologtostderr = true;
+    int a = 1;
+    int b = 3;
+    LOG(INFO) << "Start test...\n";
+    return RUN_ALL_TESTS();
 }
