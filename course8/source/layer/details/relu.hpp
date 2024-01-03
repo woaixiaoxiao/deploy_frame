839b94a1865aa6a36c418da59f513fa3cf7b4415
dcao <2191279878@qq.com>
2024-01-03 16:47:11 +0800

    完成hw6

--- course8/source/layer/details/relu.hpp
+++ course8/source/layer/details/relu.hpp
@@ -18,7 +18,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-    
+
 // Created by fss on 22-11-18.
 
 #ifndef KUIPER_INFER_SOURCE_LAYER_BINOCULAR_RELU_HPP_
@@ -26,15 +26,13 @@
 #include "layer/abstract/non_param_layer.hpp"
 namespace kuiper_infer {
 class ReluLayer : public NonParamLayer {
- public:
-  ReluLayer() : NonParamLayer("Relu") {}
-  InferStatus Forward(
-      const std::vector<std::shared_ptr<Tensor<float>>>& inputs,
-      std::vector<std::shared_ptr<Tensor<float>>>& outputs) override;
+public:
+    ReluLayer() : NonParamLayer("Relu") {}
+    InferStatus Forward(const std::vector<std::shared_ptr<Tensor<float>>> &inputs,
+                        std::vector<std::shared_ptr<Tensor<float>>> &outputs) override;
 
-  static ParseParameterAttrStatus GetInstance(
-      const std::shared_ptr<RuntimeOperator>& op,
-      std::shared_ptr<Layer>& relu_layer);
+    static ParseParameterAttrStatus GetInstance(const std::shared_ptr<RuntimeOperator> &op,
+                                                std::shared_ptr<Layer> &relu_layer);
 };
-}  // namespace kuiper_infer
-#endif  // KUIPER_INFER_SOURCE_LAYER_BINOCULAR_RELU_HPP_
+} // namespace kuiper_infer
+#endif // KUIPER_INFER_SOURCE_LAYER_BINOCULAR_RELU_HPP_
